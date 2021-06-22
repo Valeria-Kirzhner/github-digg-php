2ebe9bca62eff264b7196d8ca5d5c3e0841ee43d
Valeria Kirzhner <Valeria.kirzhner@gmail.com>
2021-06-22 18:36:04 +0300

    code correction.

--- edit-post.php
+++ edit-post.php
@@ -13,7 +13,7 @@ $pid = filter_input(INPUT_GET, 'pid', FILTER_SANITIZE_STRING);
 
 if( $pid && is_numeric($pid) ){
 
-    $link = mysqli_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PWD, MYSQL_DB);
+    $link = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
     $pid = mysqli_real_escape_string($link, $pid);
     $sql = "SELECT * FROM posts WHERE id = $pid AND user_id = $uid";
     $result = mysqli_query($link, $sql);
